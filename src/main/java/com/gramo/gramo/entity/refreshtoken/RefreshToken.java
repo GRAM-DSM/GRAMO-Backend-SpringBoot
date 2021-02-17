5bc5de6178307283df0dd71256648dcc1c8c6462
Hong Jung hyeon <jhhong0509@naver.com>
2021-02-17 22:08:00 +0900

    [ADD] redis

--- src/main/java/com/gramo/gramo/entity/refreshtoken/RefreshToken.java
+++ src/main/java/com/gramo/gramo/entity/refreshtoken/RefreshToken.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -12,14 +15,16 @@ import javax.persistence.Id;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@RedisHash(value = "refresh_token")
 public class RefreshToken {
 
     @Id
     private String email;
 
+    @Indexed
     private String refreshToken;
 
+    @TimeToLive
     private Long refreshExp;
 
     public RefreshToken update(Long refreshExp) {
