98bbdbbc038af981875e29b20ff51e72e6b985e9
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-03-09 19:27:44 +0900

    [FIX] indentation

--- src/main/java/com/gramo/gramo/security/TokenFilter.java
+++ src/main/java/com/gramo/gramo/security/TokenFilter.java
@@ -20,7 +20,8 @@ public class TokenFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);     // request에서 토큰 값을 추출
 
-        if(token != null && jwtTokenProvider.validateToken(token) && jwtTokenProvider.getEmailStatus(token)) {      // 만약 토큰이 null이 아니고, 유효한 토큰이고, 이메일 검증을 받은 이메일이면 실행
+        if(token != null && jwtTokenProvider.validateToken(token)
+                && jwtTokenProvider.getEmailStatus(token)) {      // 만약 토큰이 null이 아니고, 유효한 토큰이고, 이메일 검증을 받은 이메일이면 실행
             Authentication authentication = jwtTokenProvider.getAuthentication(token);                              // 토큰 값을 가져옴
             SecurityContextHolder.getContext().setAuthentication(authentication);                                   // 값을 SecurityContextHolder에 넣음
         }
--- src/main/java/com/gramo/gramo/security/auth/AuthDetails.java
+++ src/main/java/com/gramo/gramo/security/auth/AuthDetails.java
@@ -23,12 +23,12 @@ public class AuthDetails implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getEmail();
     }
 
     @Override
