a160e81e603ed738fba5e1d341e3ff6f14ef055d
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-03-24 21:52:04 +0900

    [FIX] UserController

--- src/main/java/com/gramo/gramo/controller/UserController.java
+++ src/main/java/com/gramo/gramo/controller/UserController.java
@@ -4,10 +4,12 @@ import com.gramo.gramo.payload.response.UserInfoResponse;
 import com.gramo.gramo.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
