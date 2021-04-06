571da6ac23c2762ad60baa6c3aefb715bc9efb39
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-06 20:16:12 +0900

    [FIX] security

--- src/main/java/com/gramo/gramo/security/JwtTokenProvider.java
+++ src/main/java/com/gramo/gramo/security/JwtTokenProvider.java
@@ -41,7 +41,7 @@ public class JwtTokenProvider {
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(Base64.getEncoder().encode(secretKey.getBytes())).parseClaimsJws(token).getBody().getSubject();        // 토큰을 추출했을 때, 오류가 발생하지 않으면 true 반환
+            Jwts.parser().setSigningKey(Base64.getEncoder().encodeToString(secretKey.getBytes())).parseClaimsJws(token).getBody().getSubject();        // 토큰을 추출했을 때, 오류가 발생하지 않으면 true 반환
             return true;
         } catch (Exception e) {
             throw new InvalidTokenException();
