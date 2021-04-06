2b145a658e4959eaee84709604271e7f7e045461
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-06 18:00:46 +0900

    [FIX]

--- src/main/java/com/gramo/gramo/security/JwtTokenProvider.java
+++ src/main/java/com/gramo/gramo/security/JwtTokenProvider.java
@@ -37,26 +37,6 @@ public class JwtTokenProvider {
 
     private final AuthDetailService authDetailService;
 
-    public String generateAccessToken(String email) {
-        return Jwts.builder()
-                .setIssuedAt(new Date())
-                .setSubject(email)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .setExpiration(new Date(System.currentTimeMillis() + accessTokenExp * 1000))
-                .claim("type", "access_token")
-                .compact();
-    }
-
-    public String generateRefreshToken(String email) {
-        return Jwts.builder()
-                .setIssuedAt(new Date())                                        // 생성일
-                .setSubject(email)                                              // 암호화할 것
-                .signWith(SignatureAlgorithm.HS256, secretKey)                  // 암호화 방식, 시크릿 키
-                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExp * 1000)) // 파기일
-                .claim("type", "refresh_token")                     // 나중에 이 토큰이 refresh_token인지 여부 확인을 위한 이름
-                .compact();                                                    // 적용
-    }
-
     public String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(header);             // request에서 header 부분을 추출
         if(bearerToken != null && bearerToken.startsWith(prefix)) {
@@ -67,7 +47,7 @@ public class JwtTokenProvider {
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();        // 토큰을 추출했을 때, 오류가 발생하지 않으면 true 반환
+            Jwts.parser().setSigningKey(Base64.getEncoder().encode(secretKey.getBytes(StandardCharsets.UTF_8))).parseClaimsJws(token).getBody().getSubject();        // 토큰을 추출했을 때, 오류가 발생하지 않으면 true 반환
             return true;
         } catch (Exception e) {
             throw new InvalidTokenException();
@@ -76,7 +56,7 @@ public class JwtTokenProvider {
 
     public String getEmail(String token) {
         try {
-            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject(); // 토큰에서 email만 추출해서 가져옴
+            return Jwts.parser().setSigningKey(Base64.getEncoder().encode(secretKey.getBytes(StandardCharsets.UTF_8))).parseClaimsJws(token).getBody().getSubject(); // 토큰에서 email만 추출해서 가져옴
         } catch (Exception e) {
             throw new InvalidTokenException();
         }
@@ -85,14 +65,6 @@ public class JwtTokenProvider {
     public Authentication getAuthentication(String token) {
         AuthDetails authDetails = authDetailService.loadUserByUsername(getEmail(token));                // 토큰에서 email 추출하고 그걸로 AuthDetails에 넣어줌
         return new UsernamePasswordAuthenticationToken(authDetails, "", authDetails.getAuthorities());  // 인증용 객체 생성
-    }
-
-    public boolean isRefreshToken(String token) {
-        try {
-            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("type").equals("refresh_token");      // 토큰 생성할 때 같이 저장한 토큰 종류를 가져와서 refresh_token인지 판단
-        } catch (Exception e) {
-            throw new InvalidTokenException();
-        }
     }
 
     public boolean getEmailStatus(String token) {
