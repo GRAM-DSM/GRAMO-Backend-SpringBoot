60454e1683feeec3b2838f850db7f17943fd4e1d
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-15 20:50:18 +0900

    [UPDATE] UserRepository findAll()

--- src/main/java/com/gramo/gramo/entity/user/UserRepository.java
+++ src/main/java/com/gramo/gramo/entity/user/UserRepository.java
@@ -2,8 +2,10 @@ package com.gramo.gramo.entity.user;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, String> {
     Optional<User> findByEmail(String email);
+    List<User> findAllBy();
 }
