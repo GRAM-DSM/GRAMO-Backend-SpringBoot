0a0538005b38111a4821771c558fc690771969aa
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-15 19:41:20 +0900

    [REFACTORING] code

--- src/main/java/com/gramo/gramo/service/homework/HomeworkServiceImpl.java
+++ src/main/java/com/gramo/gramo/service/homework/HomeworkServiceImpl.java
@@ -2,6 +2,7 @@ package com.gramo.gramo.service.homework;
 
 import com.gramo.gramo.entity.homework.Homework;
 import com.gramo.gramo.entity.homework.HomeworkRepository;
+import com.gramo.gramo.entity.homework.embedded.Term;
 import com.gramo.gramo.entity.user.User;
 import com.gramo.gramo.exception.HomeworkNotFoundException;
 import com.gramo.gramo.exception.LoginException;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,17 +28,21 @@ public class HomeworkServiceImpl implements HomeworkService {
     @Override
     public void saveHomework(HomeworkRequest homeworkRequest) {
 
+        Term term = Term.builder()
+                .endDate(homeworkRequest.getEndDate())
+                .startDate(LocalDate.now())
+                .build();
+
         homeworkRepository.save(
                 Homework.builder()
                         .teacherEmail(authenticationFacade.getUserEmail())
                         .description(homeworkRequest.getDescription())
-                        .endDate(homeworkRequest.getEndDate())
                         .isRejected(false)
                         .major(homeworkRequest.getMajor())
                         .isSubmitted(false)
-                        .startDate(LocalDate.now())
                         .studentEmail(homeworkRequest.getStudentEmail())
                         .title(homeworkRequest.getTitle())
+                        .term(term)
                         .build()
         );
 
@@ -58,94 +64,29 @@ public class HomeworkServiceImpl implements HomeworkService {
     @Override
     public HomeworkResponse getHomework(Long homeworkId) {
 
-        Homework homework = homeworkRepository.findById(homeworkId)
-                .orElseThrow(HomeworkNotFoundException::new);
+        return buildResponse(
+                homeworkRepository.findById(homeworkId)
+                        .orElseThrow(HomeworkNotFoundException::new)
+        );
 
-        return HomeworkResponse.builder()
-                .major(homework.getMajor())
-                .teacherName(homework.getTeacherEmail())
-                .studentName(homework.getStudentEmail())
-                .isRejected(homework.getIsRejected())
-                .startDate(homework.getStartDate())
-                .endDate(homework.getEndDate())
-                .description(homework.getDescription())
-                .title(homework.getTitle())
-                .build();
     }
 
     @Override
     public List<HomeworkResponse> getAssignedHomework() {
 
-        List<Homework> homework = homeworkRepository.findAllByIsSubmittedFalseAndStudentEmailOrderByIdDesc(authenticationFacade.getUserEmail());
-        List<HomeworkResponse> homeworkResponses = new ArrayList<>();
+        return buildResponseList(homeworkRepository.findAllByIsSubmittedFalseAndStudentEmailOrderByIdDesc(authenticationFacade.getUserEmail()));
 
-        for(Homework work : homework) {
-            homeworkResponses.add(
-                    HomeworkResponse.builder()
-                            .homeworkId(work.getId())
-                            .description(work.getDescription())
-                            .endDate(work.getEndDate())
-                            .startDate(work.getStartDate())
-                            .isRejected(work.getIsRejected())
-                            .major(work.getMajor())
-                            .studentName(work.getStudentEmail())
-                            .teacherName(work.getTeacherEmail())
-                            .title(work.getTitle())
-                            .build()
-            );
-        }
-
-        return homeworkResponses;
     }
 
     @Override
     public List<HomeworkResponse> getSubmittedHomework() {
 
-        List<Homework> homework = homeworkRepository.findAllByIsSubmittedTrueAndStudentEmailOrderByIdDesc(authenticationFacade.getUserEmail());
-        List<HomeworkResponse> homeworkResponses = new ArrayList<>();
-
-        for(Homework work : homework) {
-            homeworkResponses.add(
-                    HomeworkResponse.builder()
-                            .homeworkId(work.getId())
-                            .description(work.getDescription())
-                            .endDate(work.getEndDate())
-                            .startDate(work.getStartDate())
-                            .isRejected(work.getIsRejected())
-                            .major(work.getMajor())
-                            .studentName(work.getStudentEmail())
-                            .teacherName(work.getTeacherEmail())
-                            .title(work.getTitle())
-                            .build()
-            );
-        }
-
-        return homeworkResponses;
+        return buildResponseList(homeworkRepository.findAllByIsSubmittedTrueAndStudentEmailOrderByIdDesc(authenticationFacade.getUserEmail()));
     }
 
     @Override
     public List<HomeworkResponse> getOrderedHomework() {
-
-        List<Homework> homework = homeworkRepository.findAllByTeacherEmailOrderByIdDesc(authenticationFacade.getUserEmail());
-        List<HomeworkResponse> homeworkResponses = new ArrayList<>();
-
-        for(Homework work : homework) {
-            homeworkResponses.add(
-                    HomeworkResponse.builder()
-                            .homeworkId(work.getId())
-                            .description(work.getDescription())
-                            .endDate(work.getEndDate())
-                            .startDate(work.getStartDate())
-                            .isRejected(work.getIsRejected())
-                            .major(work.getMajor())
-                            .studentName(work.getStudentEmail())
-                            .teacherName(work.getTeacherEmail())
-                            .title(work.getTitle())
-                            .build()
-            );
-        }
-
-        return homeworkResponses;
+        return buildResponseList(homeworkRepository.findAllByTeacherEmailOrderByIdDesc(authenticationFacade.getUserEmail()));
     }
 
     @Override
@@ -172,5 +113,29 @@ public class HomeworkServiceImpl implements HomeworkService {
         }
 
         homeworkRepository.save(homework.rejectHomework());
+    }
+
+    private List<HomeworkResponse> buildResponseList(List<Homework> homeworkList) {
+        List<HomeworkResponse> homeworkResponses = new ArrayList<>();
+
+        for(Homework work : homeworkList) {
+            homeworkResponses.add(buildResponse(work));
+        }
+
+        return homeworkResponses;
+    }
+
+    private HomeworkResponse buildResponse(Homework homework) {
+        return HomeworkResponse.builder()
+                .homeworkId(homework.getId())
+                .description(homework.getDescription())
+                .endDate(homework.getTerm().getEndDate())
+                .startDate(homework.getTerm().getStartDate())
+                .isRejected(homework.getIsRejected())
+                .major(homework.getMajor())
+                .studentName(homework.getStudentEmail())
+                .teacherName(homework.getTeacherEmail())
+                .title(homework.getTitle())
+                .build();
     }
 }
