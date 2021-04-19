bbb17e30694b0f62797d9b142ff584b3b55d012e
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-19 14:16:42 +0900

    [REFACTOR] code

--- src/main/java/com/gramo/gramo/service/calendar/CalendarServiceImpl.java
+++ src/main/java/com/gramo/gramo/service/calendar/CalendarServiceImpl.java
@@ -1,15 +1,19 @@
 package com.gramo.gramo.service.calendar;
 
+import com.gramo.gramo.entity.BasicCalendarRepository;
+import com.gramo.gramo.entity.picu.Picu;
 import com.gramo.gramo.entity.picu.PicuRepository;
+import com.gramo.gramo.entity.plan.Plan;
 import com.gramo.gramo.entity.plan.PlanRepository;
 import com.gramo.gramo.payload.response.CalendarContentResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.jni.Local;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -20,22 +24,77 @@ public class CalendarServiceImpl implements CalendarService {
 
     @Override
     public List<CalendarContentResponse> getCalendar(LocalDate date) {
+        HashMap<LocalDate, Integer> picuMap = new HashMap<>();
+        HashMap<LocalDate, Integer> planMap = new HashMap<>();
+
+        Map<String, LocalDate> dateMap = getDate(date);
+
+        List<Picu> picus = picuRepository.findAllByDateBetween(dateMap.get("startDate"), dateMap.get("endDate"));
+        List<Plan> plans = planRepository.findAllByDateBetween(dateMap.get("startDate"), dateMap.get("endDate"));
+
+        picuMap = getPicuMap(picus, picuMap);
+        planMap = getPlanMap(plans, planMap);
+
+        return buildResponse(picuMap, planMap, dateMap);
+    }
+
+    private List<CalendarContentResponse> buildResponse(HashMap<LocalDate, Integer> picuMap,
+                                                        HashMap<LocalDate, Integer> planMap,
+                                                        Map<String, LocalDate> dateMap) {
+
+        LocalDate current = dateMap.get("startDate");
+
         List<CalendarContentResponse> calendarContentResponses = new ArrayList<>();
 
-        date.getDayOfMonth();
-        LocalDate localDate = LocalDate.of(date.getYear(), date.getMonth(), 1);
-        LocalDate currentDate = LocalDate.of(date.getYear(),date.getMonth(),date.lengthOfMonth());
-        while (currentDate.isAfter(localDate) || currentDate.isEqual(localDate)) {
+        while(!current.isAfter(dateMap.get("endDate"))) {
             calendarContentResponses.add(
                     CalendarContentResponse.builder()
-                            .date(localDate)
-                            .picuCount(picuRepository.countAllByDate(localDate))
-                            .planCount(planRepository.countAllByDate(localDate))
+                            .picuCount(picuMap.get(current) != null ? picuMap.get(current).longValue(): 0)
+                            .planCount(planMap.get(current) != null ? planMap.get(current).longValue(): 0)
+                            .date(current)
                             .build()
             );
-            localDate = localDate.plusDays(1);
+            current = current.plusDays(1L);
         }
 
         return calendarContentResponses;
+
+    }
+
+    private HashMap<LocalDate, Integer> getPicuMap(List<Picu> picus,
+                                                           HashMap<LocalDate, Integer> picuMap) {
+
+        for(Picu picu : picus) {
+            if(picuMap.get(picu.getDate()) == null) {
+                picuMap.put(picu.getDate(), 1);
+            } else {
+                picuMap.put(picu.getDate(), picuMap.get(picu.getDate()) + 1);
+            }
+        }
+        return picuMap;
+    }
+
+    private HashMap<LocalDate, Integer> getPlanMap(List<Plan> plans,
+                                                   HashMap<LocalDate, Integer> planMap) {
+        for (Plan plan : plans) {
+            if (planMap.get(plan.getDate()) == null) {
+                planMap.put(plan.getDate(), 1);
+            } else {
+                planMap.put(plan.getDate(), planMap.get(plan.getDate()) + 1);
+            }
+        }
+        return planMap;
+    }
+
+    private Map<String, LocalDate> getDate(LocalDate date) {
+        Map<String, LocalDate> dateMap = new HashMap<>();
+
+        LocalDate startDate = LocalDate.of(date.getYear(), date.getMonth(), 1);
+        LocalDate endDate = LocalDate.of(date.getYear(), date.getMonth(), date.lengthOfMonth());
+
+        dateMap.put("startDate", startDate);
+        dateMap.put("endDate", endDate);
+
+        return dateMap;
     }
 }
--- src/test/java/com/gramo/gramo/controller/CalendarControllerTest.java
+++ src/test/java/com/gramo/gramo/controller/CalendarControllerTest.java
@@ -1,2 +1,134 @@
-package com.gramo.gramo.controller;public class CalendarControllerTest {
+package com.gramo.gramo.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.gramo.gramo.GramoApplication;
+import com.gramo.gramo.entity.picu.Picu;
+import com.gramo.gramo.entity.picu.PicuRepository;
+import com.gramo.gramo.entity.plan.Plan;
+import com.gramo.gramo.entity.plan.PlanRepository;
+import com.gramo.gramo.entity.user.User;
+import com.gramo.gramo.entity.user.UserRepository;
+import com.gramo.gramo.entity.user.enums.Major;
+import com.gramo.gramo.payload.response.CalendarContentResponse;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ContextConfiguration(classes = GramoApplication.class)
+@ActiveProfiles({"test"})
+public class CalendarControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private PicuRepository picuRepository;
+
+    @Autowired
+    private PlanRepository planRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setUp() {
+        this.mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+
+        User user = createUser();
+
+        createPicu(user);
+        createPicu(user);
+        createPicu(user);
+
+        createPlan(user);
+        createPlan(user);
+        createPlan(user);
+        createPlan(user);
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        planRepository.deleteAll();
+        picuRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @Test
+    @WithMockUser(value = "email", password = "pwd")
+    public void calendar_list_test() throws Exception {
+        MvcResult result = mvc.perform(get("/calendar")
+                .param("date", "2021-04-19"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        List<CalendarContentResponse> responses = new ObjectMapper().registerModule(new JavaTimeModule())
+                .readValue(result.getResponse().getContentAsString(), new TypeReference<List<CalendarContentResponse>>() {});
+
+        Assertions.assertEquals(responses.get(0).getDate(), LocalDate.of(2021, 04, 01));
+        Assertions.assertEquals(responses.get(0).getPicuCount(), 3);
+        Assertions.assertEquals(responses.get(0).getPlanCount(), 4);
+
+
+    }
+
+    private User createUser() {
+        return userRepository.save(
+                User.builder()
+                        .password(passwordEncoder.encode("pwd"))
+                        .name("name")
+                        .major(Major.BACKEND)
+                        .emailStatus(true)
+                        .email("email")
+                        .build()
+        );
+
+    }
+
+    private Picu createPicu(User user) {
+        return picuRepository.save(
+                Picu.builder()
+                        .date(LocalDate.of(2021,04,1))
+                        .description("desc")
+                        .userEmail(user.getEmail())
+                        .build()
+        );
+    }
+
+    private Plan createPlan(User user) {
+        return planRepository.save(
+                Plan.builder()
+                        .date(LocalDate.of(2021,04,1))
+                        .description("desc")
+                        .title("title")
+                        .build()
+        );
+    }
 }
