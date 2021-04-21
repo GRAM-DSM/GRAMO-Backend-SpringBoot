e790be0b840931e42182e5b13ae3914840181c58
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-22 08:46:17 +0900

    [ADD] BasicMapperConfig

---
+++ src/main/java/com/gramo/gramo/mapper/BasicMapperConfig.java
@@ -0,0 +1,10 @@
+package com.gramo.gramo.mapper;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(componentModel = "spring")      // 이게 있어야 Spring Bean에 등록.
+// 매핑 안된 필드 무시는, build.gradle에서 활성화 되어 있어서 안해도 된다.
+public interface BasicMapperConfig {
+
+}
