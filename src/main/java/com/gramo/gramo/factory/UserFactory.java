fe3c0ecab690ff3656e2ec8e1d5e842c24a350df
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-26 21:57:07 +0900

    [FIX] UserFactory

--- src/main/java/com/gramo/gramo/factory/UserFactory.java
+++ src/main/java/com/gramo/gramo/factory/UserFactory.java
@@ -1,2 +1,31 @@
-package com.gramo.gramo.factory;public class UserFactory {
+package com.gramo.gramo.factory;
+
+import com.gramo.gramo.entity.user.User;
+import com.gramo.gramo.entity.user.UserRepository;
+import com.gramo.gramo.exception.UserNotFoundException;
+import com.gramo.gramo.security.auth.AuthenticationFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+
+@Component
+@RequiredArgsConstructor
+public class UserFactory {
+
+    private final UserRepository userRepository;
+    private final AuthenticationFacade authenticationFacade;
+
+    public User getUser(String email) {
+        return findUser(email);
+    }
+
+    public User getAuthUser() {
+        return findUser(authenticationFacade.getUserEmail());
+    }
+
+    private User findUser(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+    }
 }
