4f5c4609cdd4e4d476810cc79b447407ce444fa3
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-26 23:14:06 +0900

    [FIX] controller query parameter to path parameter

--- src/main/java/com/gramo/gramo/controller/CalendarController.java
+++ src/main/java/com/gramo/gramo/controller/CalendarController.java
@@ -4,10 +4,7 @@ import com.gramo.gramo.payload.response.CalendarContentResponse;
 import com.gramo.gramo.service.calendar.CalendarService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -18,8 +15,8 @@ import java.util.List;
 public class CalendarController {
 
     private final CalendarService calendarService;
-    @GetMapping
-    public List<CalendarContentResponse> getCalendar(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
+    @GetMapping("/{date}")
+    public List<CalendarContentResponse> getCalendar(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
         return calendarService.getCalendar(date);
     }
 }
