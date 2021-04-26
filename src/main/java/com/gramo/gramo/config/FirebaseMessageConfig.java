a2d2202b8f4a16fe2787ea9cccc85c2991392e8b
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-26 21:55:47 +0900

    [ADD] firebaseConfig

--- src/main/java/com/gramo/gramo/config/FirebaseMessageConfig.java
+++ src/main/java/com/gramo/gramo/config/FirebaseMessageConfig.java
@@ -1,2 +1,38 @@
-package com.gramo.gramo.config;public class FirebaseMessageConfig {
+package com.gramo.gramo.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+@Configuration
+@RequiredArgsConstructor
+public class FirebaseMessageConfig {
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging() throws IOException {
+        try {
+            GoogleCredentials googleCredentials = GoogleCredentials
+                    .fromStream(new ClassPathResource("firebase-service-account.json").getInputStream());
+            FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                    .setCredentials(googleCredentials)              // apply credentials on options
+                    .build();
+            FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "gramo");
+            return FirebaseMessaging.getInstance(app);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
 }
