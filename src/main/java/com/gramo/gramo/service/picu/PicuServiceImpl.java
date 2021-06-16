d46f2937a52d1223427028c07e99e624c5306b20
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-06-16 16:51:05 +0900

    [TEST]

--- src/main/java/com/gramo/gramo/service/picu/PicuServiceImpl.java
+++ src/main/java/com/gramo/gramo/service/picu/PicuServiceImpl.java
@@ -1,6 +1,8 @@
 package com.gramo.gramo.service.picu;
 
+import com.gramo.gramo.entity.picu.Picu;
 import com.gramo.gramo.entity.picu.PicuRepository;
+import com.gramo.gramo.exception.PermissionMismatchException;
 import com.gramo.gramo.exception.PicuNotFoundException;
 import com.gramo.gramo.factory.UserFactory;
 import com.gramo.gramo.mapper.PicuMapper;
@@ -39,9 +41,13 @@ public class PicuServiceImpl implements PicuService{
 
     @Override
     public void deletePicu(Long picuId) {
-        picuRepository.findById(picuId)
-                .filter((var picu) -> picu.getUserEmail().equals(authenticationFacade.getUserEmail()))
-                .ifPresentOrElse(picuRepository::delete, PicuNotFoundException::new);
+        Picu picu = picuRepository.findById(picuId)
+                .orElseThrow(PicuNotFoundException::new);
+        if(!picu.getUserEmail().equals(authenticationFacade.getUserEmail())) {
+            throw new PermissionMismatchException();
+        }
+
+        picuRepository.delete(picu);
     }
 
 }
