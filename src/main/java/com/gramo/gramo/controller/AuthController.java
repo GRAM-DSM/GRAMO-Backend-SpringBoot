189ff809482aececde2d19f2ce7a1b0d4ba6f80b
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-06-17 13:31:52 +0900

    [ADD] auth

---
+++ src/main/java/com/gramo/gramo/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.gramo.gramo.controller;
+
+import com.google.api.client.auth.oauth2.TokenRequest;
+import com.gramo.gramo.payload.request.AuthRequest;
+import com.gramo.gramo.payload.response.TokenRefreshResponse;
+import com.gramo.gramo.payload.response.TokenResponse;
+import com.gramo.gramo.service.auth.AuthService;
+import com.gramo.gramo.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/auth")
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TokenResponse signIn(@RequestBody AuthRequest request) {
+        return authService.signIn(request);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TokenRefreshResponse tokenRefresh(@RequestHeader(name = "Authorization") String refreshToken) {
+        return authService.tokenRefresh(refreshToken);
+    }
+}
