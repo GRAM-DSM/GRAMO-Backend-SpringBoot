17f6bffe648e6bd114a4f6209a0cdaabc458f2e7
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-06-17 14:41:07 +0900

    [ADD] MailStarter

--- src/main/java/com/gramo/gramo/service/email/EmailServiceImpl.java
+++ src/main/java/com/gramo/gramo/service/email/EmailServiceImpl.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
