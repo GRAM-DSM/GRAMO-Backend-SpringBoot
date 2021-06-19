5b891d7083630e111a072c67e7c2ba7e225fffa0
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-06-19 21:24:00 +0900

    [FIX] EntityName

--- src/main/java/com/gramo/gramo/entity/notice/Notice.java
+++ src/main/java/com/gramo/gramo/entity/notice/Notice.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @Entity
+@Table(name = "notice_tbl")
 public class Notice {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
