4114669b647eddbc290f7f3942f70e213af16c7b
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-07-01 18:53:03 +0900

    [ADD] @NotBlank

--- src/main/java/com/gramo/gramo/payload/request/AuthRequest.java
+++ src/main/java/com/gramo/gramo/payload/request/AuthRequest.java
@@ -4,15 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthRequest {
 
+    @NotBlank
     private String email;
 
+    @NotBlank
     private String password;
 
+    @NotBlank
     private String token;
 
 }
