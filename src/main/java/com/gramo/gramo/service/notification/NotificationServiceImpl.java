1804f0db0c64fe4efef29034c6ebef2122f12354
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-07-06 20:12:43 +0900

    [FIX]

--- src/main/java/com/gramo/gramo/service/notification/NotificationServiceImpl.java
+++ src/main/java/com/gramo/gramo/service/notification/NotificationServiceImpl.java
@@ -67,7 +67,6 @@ public class NotificationServiceImpl implements NotificationService {
                             .putData("content", data.getMessage())
                             .build())
                     .build();
-
             FirebaseMessaging.getInstance().sendAsync(message);
         } catch (Exception e) {
             throw new SendNotificationFailed();
